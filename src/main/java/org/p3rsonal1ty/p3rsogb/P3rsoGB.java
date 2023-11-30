e4da74b5c576bc65d0713c8d7a14f513b6736897
P3rsonal1ty <146423139+P3rsonal1ty@users.noreply.github.com>
2023-12-01 01:55:09 +0300

    Minecraft

---
+++ src/main/java/org/p3rsonal1ty/p3rsogb/P3rsoGB.java
@@ -0,0 +1,17 @@
+package org.p3rsonal1ty.p3rsogb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class P3rsoGB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
